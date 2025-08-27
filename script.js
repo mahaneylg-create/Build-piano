dd2e16523476b15dad77b2b10d0031623bb9ba2b
mahaneylg-create <mahaneylg@vcu.edu>
2025-08-26 22:02:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ keys.forEach((key) => {
 function playNote(key) {
   const noteAudio = document.getElementById(key.dataset.note);
   noteAudio.currentTime = 0;
-  noteAudio.play();
-  key.classList.add('active');  
-}
+  noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active');
+  });
